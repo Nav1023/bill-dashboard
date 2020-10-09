639fd4f62c7898cc3be346b4f7e1e3ed7b19de03
Navneet Singh <rathore.singhnav@gmail.com>
2020-10-10 02:25:23 +0530

    Added minimum bills

---
+++ src/components/MinimumBills/index.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import BillCard from "../BillCard";
+import "./index.css";
+
+const MinimumBills = props => {
+  const { billData } = props;
+  const [showMinBill, setShowMinBill] = useState(false);
+  const [monthlyBudget, setMonthlyBudget] = useState(0);
+
+  let billList = [...billData];
+  //if Filter is On
+  if (showMinBill && monthlyBudget) {
+    billList.sort((firstBill, secondBill) => {
+      const x = Number(firstBill.amount);
+      const y = Number(secondBill.amount);
+      return x > y ? -1 : x < y ? 1 : 0;
+    });
+    let currentBillAmount = 0;
+    billList = billList.filter(bill => {
+      if (currentBillAmount + Number(bill.amount) <= monthlyBudget) {
+        currentBillAmount += Number(bill.amount);
+        return bill;
+      }
+    });
+  }
+  return (
+    <div className="bill-list-wrapper">
+      <p className="heading">Your Bills</p>
+      <div className="bill-list-header">
+        <div>
+          <input
+            onChange={e => { setMonthlyBudget(e.target.value); setShowMinBill(false)}}
+            name="budget"
+            className="input-field"
+            type="text"
+            value = {monthlyBudget}
+            placeholder="Enter Budget"
+          />
+          <button onClick={() => setShowMinBill(true)}>Set</button>
+        </div>
+        <button onClick={() => {setShowMinBill(false); setMonthlyBudget(0)}}>Reset</button>
+      </div>
+
+      <div className="bill-list-container">
+        {billData.length > 0 ? (
+          billList.map(bill => <BillCard data={bill} showCardActions={false} />)
+        ) : (
+          <p>No Bills</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+MinimumBills.propTypes = {
+  props: PropTypes
+};
+
+const mapStateToProps = state => {
+  return {
+    billData: state.BillState.billData
+  };
+};
+
+export default connect(mapStateToProps)(MinimumBills);
