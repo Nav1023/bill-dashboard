3a8cc2e94949feccd83512c2fd8782c2e8a100a7
Navneet Singh <rathore.singhnav@gmail.com>
2020-10-10 02:25:11 +0530

    Added Dashboard

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import MonthlyChart from '../MonthlyChart';
+import BillList from '../BillList';
+import 'react-tabs/style/react-tabs.css';
+import MinimumBills from '../MinimumBills';
+
+function Dashboard() {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Your Bills</Tab>
+        <Tab>Monthly Bill Graph</Tab>
+        <Tab>Minimum Bills </Tab>
+      </TabList>
+  
+      <TabPanel>
+        <BillList/>
+      </TabPanel>
+      <TabPanel>
+        <MonthlyChart/>
+      </TabPanel>
+      <TabPanel>
+       <MinimumBills/>
+      </TabPanel>
+  </Tabs>
+  )
+}
+
+export default Dashboard
